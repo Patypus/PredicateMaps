3ef1ec336b8b43cf9c4af99e268b76df38367c2b
Pat Howell <patrick.howell@outlook.com>
2014-12-30 20:47:54 +0000

    Set up directory structure and the main map class

---
+++ PredicateMaps/Maps/PredicateMap.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredicateMaps.Maps
+{
+    public class PredicateMap<K, V>
+    {
+        private readonly List<Predicate<K>> keyPredicateList;
+        private readonly List<V> valueList;
+
+        public PredicateMap()
+        {
+            keyPredicateList = new List<Predicate<K>>();
+            valueList = new List<V>();
+        }
+
+
+
+    }
+}
---
+++ PredicateMapsTests/Maps/PredicateMapTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredicateMapsTests.Maps
+{
+    [TestClass]
+    public class PredicateMapTests
+    {
+
+
+    }
+}
