fcd0986a0796c725aa4803ee7cfddab3cbc954bd
Pat Howell <patrick.howell@outlook.com>
2015-03-17 22:03:27 +0000

    Initial implementation of FizzBuzz example. needs commenting and linking up to be called still.

---
+++ Examples/FizzBuzz/FizzBuzzRunner.cs
@@ -0,0 +1,45 @@
+﻿using PredicateMaps.Maps;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Examples.FizzBuzz
+{
+    public class FizzBuzzRunner
+    {
+        private readonly IPredicateMap<int, string> fizzBuzzSequenceMap;
+
+        public FizzBuzzRunner()
+        {
+            fizzBuzzSequenceMap = SetupSequenceMap();
+        }
+
+        public IList<string> Run(int numberToCountTo)
+        {
+            var result = new List<string>();
+            for (var x = 0; x <= numberToCountTo; x++ )
+            {
+                var matches = fizzBuzzSequenceMap.GetAllMatches(x);
+                if (matches.Any()) 
+                {
+                    result.AddRange(matches);
+                }
+                else
+                {
+                    result.Add(x.ToString());
+                }
+                
+            }
+            return result;
+        }
+
+        private IPredicateMap<int, string> SetupSequenceMap()
+        {
+            var functions = new List<Predicate<int>> { i => i % 3 == 0, i => i % 5 == 0 };
+            var values = new List<string> { "fizz", "buzz" };
+            return new ClassPredicateMap<int, string>(functions, values);
+        }
+    }
+}
