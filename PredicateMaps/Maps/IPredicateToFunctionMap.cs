2055a3424bf7b789014deff8edf100b86b3a91d8
Pat Howell <patrick.howell@outlook.com>
2015-04-28 21:45:45 +0100

    Started adding infrastructure to add new map for mapping predicate to function

---
+++ PredicateMaps/Maps/IPredicateToFunctionMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredicateMaps.Maps
+{
+    /// <summary>
+    /// Interface which defines a class which maps predicates taking a parameter of type K to a function
+    /// taking a a parameter of type K and returning a value of type V. The parameter K passed to the 
+    /// predicate for test can, with implementation of this interface, be passed to the function for which
+    /// the predicate is true for further manipulation or testing.
+    /// </summary>
+    /// <typeparam name="K">Type of items to test in the key predicates and for value functions to take as parameters</typeparam>
+    /// <typeparam name="V">Return type for the value function to return</typeparam>
+    public interface IPredicateToFunctionMap<K, V>
+    {
+    }
+}
---
+++ PredicateMaps/Maps/PredicateToFunctionMap.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PredicateMaps.Maps
+{
+    /// <summary>
+    /// Implementation of the IPredicateToFunctionMap where V must be of type class.
+    /// Implementation maps predicates taking a parameter of type K to a function which takes a parameter of type K and returns 
+    /// type V. Values of type K can be tested by the predicate key and where the predicate evaluates to true the value will be passed
+    /// to the value function as its parameter and the return of type V from the matching function will be returned.
+    /// </summary>
+    /// <typeparam name="K">Type of items to test in the key predicates and for value functions to take as parameters</typeparam>
+    /// <typeparam name="V">Return type for the value function to return</typeparam>
+    public class PredicateToFunctionMap<K, V> : IPredicateToFunctionMap<K, V> where V : class
+    {
+        
+    }
+}
